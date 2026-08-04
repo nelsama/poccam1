a41c1609257cfbf24ca203a1a168289f97d6d9bf
Nelson Figueroa <nelson.nfigueroa@gmail.com>
2026-08-03 21:37:34 -0400

    Remove image registration compensation
    Revierte el commit dc30a11: elimina la compensacion de vibracion
    (correlacion de fase + shift) y sus parametros de configuracion.
    El detector vuelve al estado original; se quita scipy de dependencias.

--- main.py
+++ main.py
@@ -42,8 +42,6 @@ class MonitorBackend:
             blur_ksize=config.blur_ksize,
             marcar_cambios=config.marcar_cambios,
             frames_estables=config.frames_estables,
-            alinear_imagenes=config.alinear_imagenes,
-            max_desplazamiento=config.max_desplazamiento,
         )
         self.registrador = RegistradorEventos(config)
 
